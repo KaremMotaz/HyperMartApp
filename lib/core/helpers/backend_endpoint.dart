b27bbd8ced3c6609c4595dd65da0502c53d32b7d
Karim Motaz <karemmotaz2929@gmail.com>
2025-09-16 12:57:39 +0300

    Add pretty dio logger

--- lib/core/helpers/backend_endpoint.dart
+++ lib/core/helpers/backend_endpoint.dart
@@ -1,4 +1,4 @@
 class BackendEndpoint {
   static const baseUrl = "https://accessories-eshop.runasp.net/api/";
-  static const login = 'auth/login';
+  static const login = "auth/login";
 }
--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hyper_mart_app/core/networking/dio_factory.dart';
 import 'package:hyper_mart_app/core/services/auth_service.dart';
 import '../../features/auth/data/repos/auth_repo_imp.dart';
 import '../../features/auth/domain/auth_repo.dart';
@@ -8,7 +9,7 @@ final getIt = GetIt.instance;
 
 void setupGetIt() {
   // 🔐 Auth
-  getIt.registerLazySingleton<Dio>(() => Dio());
+  getIt.registerLazySingleton<Dio>(() => DioFactory().dio);
   getIt.registerLazySingleton<AuthService>(() => AuthService(getIt()));
   getIt.registerLazySingleton<AuthRepo>(
     () => AuthRepoImp(authService: getIt()),
